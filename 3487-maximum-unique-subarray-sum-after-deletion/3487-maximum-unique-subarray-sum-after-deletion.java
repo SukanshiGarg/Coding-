35eff3258cb254edbe8247d560503df61c816771
Sukanshi Garg <sukanshigargagra123@gmail.com>
2026-07-04 01:47:40 +0530

    Time: 1 ms (100%), Space: 44.4 MB (33.19%) - LeetHub

---
+++ 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            max = Math.max(max, num);
+
+            if (num > 0 && set.add(num)) {
+                sum += num;
+            }
+        }
+
+        return sum > 0 ? sum : max;
+    }
+}
