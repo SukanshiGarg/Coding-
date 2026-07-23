c6ec8da925d4746d9003cba780942214fbfc37a3
Sukanshi Garg <sukanshigargagra123@gmail.com>
2026-07-23 13:21:52 +0530

    Time: 6 ms (78.14%), Space: 45 MB (89.46%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.java
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -1,28 +1,27 @@
-
-class Solution { 
-    public int bagOfTokensScore(int[] tokens, int power) { 
-        Arrays.sort(tokens); 
-        int score = 0; 
-        int i = 0; 
-        int j = tokens.length - 1; 
-        int max_score = 0; 
-        
-        // Use a while loop to control pointer convergence manually
-        while (i <= j) { 
-            if (tokens[i] <= power) { 
-                power -= tokens[i]; 
-                score++; 
-                i++; // Only move the low pointer forward when we buy a token face-up
-            } else if (score >= 1) { 
-                power += tokens[j]; 
-                score--; 
-                j--; // Only move the high pointer backward when we trade for power face-down
-            } else {
-                // If we can't buy the smallest token and don't have score to trade, we are stuck
-                break; 
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int n = tokens.length;
+        int i=0;
+        int j=n-1;
+        int score=0;
+        int max_score=0;
+        while(i<=j){
+            if(power>=tokens[i]){
+                power -=tokens[i];
+                score++;
+                i++;
             }
-            max_score = Math.max(max_score, score); 
-        } 
-        return max_score; 
-    } 
+            else if(score >= 1){
+                power += tokens[j];
+                score--;
+                j--;
+            } else{
+                break;
+            }
+            max_score=Math.max(max_score,score);
+
+        }
+        return max_score;
+    }
 }
