6b56e7c293c2ac7bd10d0a6999a98ebcb2fc5637
Sukanshi Garg <sukanshigargagra123@gmail.com>
2026-07-29 14:56:20 +0530

    Time: 1 ms (99.95%), Space: 77 MB (83.07%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] arr) {
+        int csum=arr[0];
+        int osum=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+             csum=Math.max(arr[i],csum+arr[i]);
+             osum=Math.max(osum,csum);
+        }
+        return osum;
+    }
+}
