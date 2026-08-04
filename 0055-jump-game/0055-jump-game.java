9d7307f434a58a4e20209df5f95eea5cfc0572ec
Sukanshi Garg <sukanshigargagra123@gmail.com>
2026-08-04 07:12:29 +0530

    Time: 1 ms (99.94%), Space: 47.9 MB (43.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int max_index=0;
-        for(int i=0;i<n;i++){
-            if(i>max_index) return false;
-            max_index=Math.max(max_index,i+nums[i]);
-            
+        int finalPosition = nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(i+nums[i]>=finalPosition){
+                finalPosition=i;
+            }
         }
-        return true;
+
+        return finalPosition==0;
     }
+
 }
