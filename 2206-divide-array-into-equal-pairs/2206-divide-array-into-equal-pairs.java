4f79cb1384cd1dbe2611be79cec253d4c28db4f4
Sukanshi Garg <sukanshigargagra123@gmail.com>
2026-08-30 15:04:12 +0530

    Time: 5 ms (73.12%), Space: 46.4 MB (52.38%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -1,16 +1,12 @@
 class Solution {
-    public boolean divideArray(int[] arr) {
-        
-    int n = arr.length;
-        Arrays.sort(arr);
-        for(int i=0;i<=arr.length-2;i+=2){
-            if(arr[i]==arr[i+1]){
-                continue;
-            }
-            else{
-                return false;
+    public boolean divideArray(int[] nums) {
+        //hash set approach 
+        HashSet<Integer> set = new HashSet<>();
+        for(int x : nums){
+            if(!set.add(x)){
+                set.remove(x);
             }
         }
-        return true;
-}
+        return set.isEmpty();
+    }
 }
