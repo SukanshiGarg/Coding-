49f887a31697ab133ef554016712d87ac8b5c606
Sukanshi Garg <sukanshigargagra123@gmail.com>
2026-08-31 14:59:14 +0530

    Time: 3 ms (87.26%), Space: 72 MB (8.22%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,29 +1,17 @@
-/* given : average = 3  ie sum/k = threshold  so sum = threshold*k 
-so, new condition gives us sum>=threshold *k 
-
-apply the rules of fixed window size 
-
-*/
-
-
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-         int totalThreshold = k * threshold;
-         
-         int sum = 0;
+        // [2,2,2,3,5,5,5,8] k=3 threshold = 4
+        int total = threshold*k;
+        int sum = 0;
+        for(int i=0;i<k;i++){
+           sum+=arr[i];
+        }
+        int count = (sum>=total)? 1:0;
 
-         for(int i=0;i<k;i++){
-            sum += arr[i];
-         }
-
-         int count = (sum >= totalThreshold)? 1:0;
-
-         for(int i=k;i<arr.length;i++){
-            sum+=arr[i]-arr[i-k];
-            count +=(sum >= totalThreshold)? 1:0;
-         }
-
-         return count;
-
+        for(int i=k;i<arr.length;i++){
+           sum+=arr[i]-arr[i-k];
+           count+= (sum>=total)? 1:0;
+        }
+        return count;
     }
 }
