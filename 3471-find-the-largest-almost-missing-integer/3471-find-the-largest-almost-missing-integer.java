2720b5b039a02af646c8b1252a75b82df21fd693
Sukanshi Garg <sukanshigargagra123@gmail.com>
2026-08-31 14:21:50 +0530

    Time: 6 ms (18.72%), Space: 46.6 MB (7.05%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,29 +1,27 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i <= nums.length - k; i++) {
-
-            HashSet<Integer> set = new HashSet<>();
-
-            for (int j = i; j < i + k; j++) {
-                set.add(nums[j]);
-            }
-
-            for (int num : set) {
-                map.put(num, map.getOrDefault(num, 0) + 1);
-            }
+      /*[3,9,2,1,7]  k=3 
+      Basically finding in the window of size 3 how many times a particular element is coming 
+      */  
+      HashMap<Integer,Integer> map = new HashMap<>();
+      for(int i=0;i<= nums.length-k;i++){
+        HashSet<Integer> set = new HashSet<>();
+        for(int j=i;j<i+k;j++){
+            set.add(nums[j]);
         }
-
-        int ans = -1;
-
-        for (int num : map.keySet()) {
-            if (map.get(num) == 1) {
-                ans = Math.max(ans, num);
-            }
+        for(int ele: set){
+            map.put(ele,map.getOrDefault(ele,0)+1);
         }
+      }
 
-        return ans;
+      //now check for the largest element that has occuured only once in the array
+      int ans = -1;
+      for(int ele : map.keySet()){
+          if(map.get(ele)==1){
+            ans=Math.max(ele,ans);
+          }
+      }
+      return ans;
+
     }
 }
